e42cc1d060874634d0a227254e7a98eca686aece
Alex <alexaumh@gmail.com>
2021-10-27 08:16:23 +1100

    Update function name to be easier to understand and improve formatting

--- apps/api/src/app/referrals/validation.ts
+++ apps/api/src/app/referrals/validation.ts
@@ -2,10 +2,13 @@ import prisma from '../prisma';
 import { check, validationResult } from 'express-validator';
 import { Request } from 'express';
 
-export const validateData = async (req: Request) => {
-    await check('givenName', 'Given name is required and must be between 2 to 200 characters long').isLength({ min: 2, max: 200}).run(req);
-    await check('surName', 'Surname is required and must be between 2 to 200 characters long').isLength({ min: 2, max: 200}).run(req);
+export const validateReferrerData = async (req: Request) => {
+    await check('givenName', 'Given name is required and must be between 2 to 200 characters long').isLength({ min: 2, max: 200 }).run(req);
+
+    await check('surName', 'Surname is required and must be between 2 to 200 characters long').isLength({ min: 2, max: 200 }).run(req);
+
     await check('email', 'Invalid email').isEmail().run(req);
+
     await check('email', 'Email is already in use').custom(async (value) => {
       const emailInUse = await prisma.referral.findFirst({
         where: { email: value },
@@ -14,6 +17,7 @@ export const validateData = async (req: Request) => {
         throw new Error();
       }
     }).run(req);
+
     await check('phone', 'Invalid Australia phone number').custom(async (value) => {
       const regex = RegExp(/^\d{8}$|^\d{10}$/);
       if (!regex.exec(value)) throw new Error();
