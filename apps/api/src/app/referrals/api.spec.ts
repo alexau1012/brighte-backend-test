681bdcbae79fed9c45e0f34656605bf69f2017fc
Alex <alexaumh@gmail.com>
2021-10-27 11:00:48 +1100

    Add unit test for getting one referral

--- apps/api/src/app/referrals/api.spec.ts
+++ apps/api/src/app/referrals/api.spec.ts
@@ -9,4 +9,19 @@ describe('Referrals API', () => {
     expect(result.status).toEqual(200);
     expect(Array.isArray(result.body)).toBe(true);
   });
+
+  it('should return a referral as an object', async () => {
+    const result = await request(app).get('/referrals/1');
+
+    expect(result.status).toEqual(200);
+    expect(result.body).toEqual(expect.objectContaining({
+      id: expect.any(Number),
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+      givenName: expect.any(String),
+      surName: expect.any(String),
+      email: expect.any(String),
+      phone: expect.any(String),
+    }));
+  })
 });
